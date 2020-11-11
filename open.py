a74ef0470fac27ac3cc14bf49ac1575b1023d68e
iKittyCode <kids@Himanis-MacBook-Pro.local>
2020-11-11 16:29:01 -0500

    adding stuff

--- open.py
+++ open.py
@@ -1,6 +1,6 @@
 
 from subprocess import *
-openWhat = input('What app do you want to open')
+openWhat = input('What app do you want to open ')
 def open():
     call(['/usr/bin/open', '/Applications/' + openWhat + ".app"])
 open()
