c5d672833ed2cae94509dae3f42f39f031280899
iKittyCode <gator.syon@gmail.com>
2020-11-21 15:15:35 -0500

    changed name of function and used main to invoke usr/bin/open

---
+++ test_open.py
@@ -0,0 +1,8 @@
+from subprocess import *
+
+openWhat = input('What app do you want to open ')
+def main():
+    call(['/usr/bin/open', '/Applications/' + openWhat + ".app"]) 
+
+if __name__ == "__main__":
+     main()
